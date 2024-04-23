87a045c16415dc243e12ca073520e52b6246822d
Katrin Ansper <ansper.katrin@gmail.com>
2024-04-23 20:50:09 +0300

    WEATHER

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
